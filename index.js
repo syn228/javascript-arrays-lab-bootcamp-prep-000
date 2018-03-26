3304d45c7348476ba622de8c85244c6dbcacfb63
Aaron Na <syn228@nyu.edu>
2018-03-26 20:22:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,4 +10,7 @@ function destructivelyPrependKitten(name){
 function destructivelyRemoveLastKitten(name){
   kittens.pop(name);
   return kittens
+}function destructivelyRemoveFirstKitten(name){
+  kittens.pop(name);
+  return kittens
 }
